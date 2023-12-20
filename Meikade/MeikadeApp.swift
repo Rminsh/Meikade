91c8691c741c4158bca00918ddfc7a2c1945c479
Armin <armin536@gmail.com>
2023-12-20 12:48:13 +0330

    Initial Commit

---
+++ Meikade/MeikadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeikadeApp.swift
+//  Meikade
+//
+//  Created by Armin on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MeikadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
