e45d10447a52ab8e93fa1eebd290065a66214e43
Armin <armin536@gmail.com>
2024-07-15 12:47:14 +0330

    Added support for watchOS - EmptyStateView

--- App/EmptyState/EmptyStateView.swift
+++ App/EmptyState/EmptyStateView.swift
@@ -22,16 +22,31 @@ struct EmptyStateView: View {
             if #available(iOS 17.0, macOS 14.0, visionOS 1.0, watchOS 10.0, *) {
                 ContentUnavailableView {
                     Label(title, systemImage: icon)
+                        #if os(watchOS)
+                        .font(.customFont(style: .title3))
+                        #else
                         .font(.customFont(style: .largeTitle))
+                        #endif
                 } description: {
                     Text(description)
+                        #if os(watchOS)
+                        .font(.customFont(style: .body))
+                        #else
                         .font(.customFont(style: .headline))
+                        #endif
                 } actions: {
                     if let actionTitle, let action, showAction {
                         Button(action: action) {
                             Text(actionTitle)
                                 .font(.customFont(style: .body))
+                                #if os(watchOS)
+                                .padding(.horizontal)
+                                #endif
                         }
+                        #if os(watchOS)
+                        .tint(.accentColor)
+                        .buttonStyle(.bordered)
+                        #endif
                     }
                 }
             } else {
@@ -84,6 +99,8 @@ struct EmptyStateView: View {
     EmptyStateView(
         icon: "person.bust",
         title: "Poet not found",
-        description: "Poet not found"
+        description: "Poet not found",
+        actionTitle: "Try again",
+        action: { print("Hello, World!") }
     )
 }
