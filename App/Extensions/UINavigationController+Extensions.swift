8ebbd03f6fbbc4fe3cbe5b3c4d1103b278d546ca
Armin <armin536@gmail.com>
2025-06-11 00:39:37 +0330

    Added spacing for tabbar text

--- App/Extensions/UINavigationController+Extensions.swift
+++ App/Extensions/UINavigationController+Extensions.swift
@@ -17,12 +17,19 @@ extension UINavigationController {
         // Tabbar custom appearance
         let tabBarAppearance = UITabBarAppearance()
         let fontAttribute: [NSAttributedString.Key : Any] = [.font: UXFont.custom(style: .caption2)]
+        let offset: UIOffset = UIOffset(horizontal: 0, vertical: 5)
         tabBarAppearance.stackedLayoutAppearance.selected.titleTextAttributes = fontAttribute
+        tabBarAppearance.stackedLayoutAppearance.selected.titlePositionAdjustment = offset
         tabBarAppearance.stackedLayoutAppearance.normal.titleTextAttributes = fontAttribute
+        tabBarAppearance.stackedLayoutAppearance.normal.titlePositionAdjustment = offset
         tabBarAppearance.compactInlineLayoutAppearance.normal.titleTextAttributes = fontAttribute
+        tabBarAppearance.compactInlineLayoutAppearance.normal.titlePositionAdjustment = offset
         tabBarAppearance.compactInlineLayoutAppearance.selected.titleTextAttributes = fontAttribute
+        tabBarAppearance.compactInlineLayoutAppearance.selected.titlePositionAdjustment = offset
         tabBarAppearance.inlineLayoutAppearance.normal.titleTextAttributes = fontAttribute
+        tabBarAppearance.inlineLayoutAppearance.normal.titlePositionAdjustment = offset
         tabBarAppearance.inlineLayoutAppearance.selected.titleTextAttributes = fontAttribute
+        tabBarAppearance.inlineLayoutAppearance.selected.titlePositionAdjustment = offset
         tabBarController?.tabBar.standardAppearance = tabBarAppearance
         tabBarController?.tabBar.scrollEdgeAppearance = tabBarAppearance
         
